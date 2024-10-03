17bd3ee439e19d3557f88ccbd4d43aec234d5491
veeveevu <142432760+veeveevu@users.noreply.github.com>
2024-10-03 15:01:19 +0300

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -11,8 +11,9 @@ connection = mysql.connector.connect(
     charset='utf8mb4',
     collation='utf8mb4_unicode_ci',)
 
-airport_id_list = list()
 sql15 = f"select airport_id from airport where is_finished = '0'"
 cursor = connection.cursor()
 cursor.execute(sql15)
-airport_id_list = cursor.fetchall()
+airport_id_tuples = cursor.fetchall()  # This returns a list of tuples.
+airport_id_list = [item[0] for item in airport_id_tuples]  # Extract airport_id from each tuple.
+print(airport_id_list)
